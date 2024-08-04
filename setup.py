cb98e6b261e9b7e0f1c94a034076502bbe7e60b0
upasanasaha <106012265+upasanasaha@users.noreply.github.com>
2024-08-04 18:00:05 +0530

    Update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='Generative AI Project',
     version = '0.0.0',
-    author = 'Upasana',
+    author = 'Upasana Saha',
     author_email='upasanasaha40@gmail.com',
     packages=find_packages(),
     install_requires=[]
